c4f5e3a721cce308bb1ef44c18c3f1bb094b6718
ETombuyses <e.tombuyses@gmail.com>
2020-03-16 11:35:03 +0100

    4 - nodemon

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', function(req, res) {
-  res.send('Qui prend quoi ?????');
+  res.send('Qui prend quoi à manger?????');
 });
 
 app.listen(port, () => console.log(`Front app listening on port ${port}!`));
