1c016211670dc251d0619fcb35c712beacb54bb2
ETombuyses <e.tombuyses@gmail.com>
2020-03-16 12:20:14 +0100

    12 - POST API

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+
+
 
 app.get('/', function(req, res) {
   res.render('index', { title: 'Qui prend quoi ?' });
 });
 
 app.post('/party', function(req, res) {
-  res.send('Post ok !');
+  console.log('params sent', req.body);
+
+  axios
+  .post(`${process.env.API_URL}/party`, req.body)
+  .then(({data}) => console.log(data))
+  .catch((err) => console.error(err));
 });
+
 
 app.listen(process.env.PORT, () => console.log(`Front app listening on port ${process.env.PORT}!`));
 
