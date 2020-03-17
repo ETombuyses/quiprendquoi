ebe84841927d1674896e633cf9153f7f853e6ec0
ETombuyses <e.tombuyses@gmail.com>
2020-03-17 11:34:48 +0100

    28 - clipboard API

--- public/script.js
+++ public/script.js
@@ -1,7 +1,4 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-"use strict";var numbers=[1,2,3],doubles=numbers.map(function(a){return 2*a});console.log(doubles);
+"use strict";navigator.clipboard?(console.log("Support du presse papier"),document.querySelectorAll("[data-clipboard]").forEach(function(a){var b=document.createElement("button");b.innerHTML="Copier",a.parentNode.append(b)})):console.warn("Pas de support :(");
 
-},{}],2:[function(require,module,exports){
-"use strict";console.log("I'ma hero too !");
-
-},{}]},{},[1,2]);
+},{}]},{},[1]);
--- scripts/test2.js
+++
@@ -1,1 +0,0 @@
-console.log("I'ma hero too !");
